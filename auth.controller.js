de7a15a2079bde5b06803178e7570d621526d240
TRIjan <trijanratnashakya@gmail.com>
2025-10-05 21:17:03 +0545

    new change

--- auth.controller.js
+++ auth.controller.js
@@ -1,5 +1,5 @@
 import User from './models/User.js';
-import { signToken } from '../middlewares/auth.js';
+import { signToken } from './auth.js';
 
 export const signup = async (req, res) => {
   try {
