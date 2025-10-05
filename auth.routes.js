ac3709966ed6ce7490d5bc19e33a599225e95bda
TRIjan <trijanratnashakya@gmail.com>
2025-10-05 21:29:37 +0545

    route of auth

--- auth.routes.js
+++ auth.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import authController from './auth.controller.js';
+import { signup, login } from './auth.controller.js';
 
 const router = Router();
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post('/signup', signup);
+router.post('/login', login);
 
 export default router;
